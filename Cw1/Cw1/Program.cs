417b687da9f8b191d3e086524a8741ce2f76e2b2
debskimich <69129514+debskimich@users.noreply.github.com>
2020-11-01 22:29:18 +0100

    Pierwszy commit

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter URL:");
+            string url = Console.ReadLine();
+
+            var httpClient = new HttpClient();
+            string responseBody = await httpClient.GetStringAsync(url);
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+            RegexOptions.IgnoreCase);
+            MatchCollection emailMatches = emailRegex.Matches(responseBody);
+
+            foreach(Match m in emailMatches)
+            {
+                Console.WriteLine(m);
+            }
+        }
+    }
+}
